adc27e510dca6ba948f0c90312387ed664aa682b
Marta Gómez Macías <marta@wazuh.com>
2018-03-24 16:21:33 +0100

    Add unit tests for configuration and input protocol validation

--- framework/tests/test_cluster.py
+++ framework/tests/test_cluster.py
@@ -7,6 +7,7 @@ import sys
 import os
 import xml.etree.ElementTree as ET
 import pytest
+import pwd
 
 sys.path.insert(0, os.path.abspath('.'))
 from wazuh import cluster
@@ -26,7 +27,7 @@ def set_config_value(config_opt, value, is_list=False):
         list(conf.find('ossec_config').find('cluster').find(config_opt))[0].text = value
     with open(common.ossec_conf, 'w') as f:
         f.write(ET.tostring(conf.find('ossec_config'), method='html'))
-    
+
 
 def test_check_cluster_status_default():
     """
@@ -34,7 +35,7 @@ def test_check_cluster_status_default():
     """
     # with default configuration, the function should return False
     assert cluster.check_cluster_status() == False, "Cluster status with default configuration"
-    
+
 
 @pytest.fixture
 def enable_disable_cluster():
@@ -47,7 +48,7 @@ def enable_disable_cluster():
 
     # restore original configuration
     set_config_value('disabled','yes')
-    
+
 
 def test_check_cluster_status_enabled(enable_disable_cluster):
     """
@@ -75,38 +76,19 @@ def test_get_status_json_enabled(enable_disable_cluster):
             "Cluster status with cluster enabled ({})".format(status)
 
 
-def test_check_cluster_config_unspecified_key():
-    """
-    Checks that the 
-    WazuhException: Error 3004 - Error in cluster configuration: Unspecified key
-    is raised when calling check_cluster_config with default configuration
-    """
+@pytest.mark.parametrize(argnames="expected_error,config_name,wconfig_val,config_val,islist", argvalues=[
+        ("Error 3004 - Error in cluster configuration: Unspecified key", 'key', '', 'a'*32, False),
+        ('Error 3004 - Error in cluster configuration: Key must be 32 characters long and only have alphanumeric characters', 'key', 'abc', 'a'*32, False),
+        ("Error 3004 - Error in cluster configuration: Invalid node type abc. Correct values are master and client", 'node_type', 'abc', 'master', False),
+        ("Error in cluster configuration: Invalid interval specification. Please, specify it with format <number>s or <number>m", 'interval', '1', '1s', False),
+        ("Error in cluster configuration: No nodes defined in cluster configuration.", 'nodes', '', '192.168.56.101', True),
+    ], scope="session")
+def test_configuration(expected_error, config_name, wconfig_val, config_val, islist):
+    set_config_value(config_name, wconfig_val, islist)
     config = cluster.read_config()
-    with pytest.raises(WazuhException, match="Error 3004 - Error in cluster configuration: Unspecified key"):
+    with pytest.raises(WazuhException, match=expected_error):
         cluster.check_cluster_config(config)
-
-
-@pytest.fixture
-def add_short_key_to_configuration():
-    """
-    Sets the cluster's key to abc
-    """
-    set_config_value('key','abc')
-
-    yield
-
-    set_config_value('key','a'*32)
-
-
-def test_check_cluster_config_short_key(add_short_key_to_configuration):
-    """
-    Checks that the
-    Error 3004 - Error in cluster configuration: Key must be 32 characters long and only have alphanumeric characters
-    is raised when calling check_cluster_configuration with a short key
-    """
-    config = cluster.read_config()
-    with pytest.raises(WazuhException, match='Error 3004 - Error in cluster configuration: Key must be 32 characters long and only have alphanumeric characters'):
-        cluster.check_cluster_config(config)
+    set_config_value(config_name, config_val, islist)
 
 
 @pytest.fixture(params=['NODE_IP', 'localhost', '0.0.0.0', '127.0.1.1'])
@@ -115,6 +97,8 @@ def add_wrong_ip_to_configuration(request):
     Add a wrong IP to the nodes configuration
     """
     set_config_value('nodes',request.param,True)
+    yield
+    set_config_value('nodes','192.168.56.101',True)
 
 
 def test_check_cluster_config_wrong_ip(add_wrong_ip_to_configuration):
@@ -126,3 +110,19 @@ def test_check_cluster_config_wrong_ip(add_wrong_ip_to_configuration):
     config = cluster.read_config()
     with pytest.raises(WazuhException, match=r'^Error 3004 - Error in cluster configuration: Invalid elements in node fields: [\w\.]+\.$'):
         cluster.check_cluster_config(config)
+
+@pytest.mark.parametrize(argnames="cmd,node_type,expected_output", argvalues=[
+    (["node","-"*(common.cluster_protocol_plain_size - len('node '))], 'client', True),
+    (["ready","-"*(common.cluster_protocol_plain_size - len('ready '))], 'master', False),
+    (["ready","-"*(common.cluster_protocol_plain_size - len('ready '))], 'client', True),
+    (["pepe","-"*(common.cluster_protocol_plain_size - len('pepe '))], 'client', False),
+    (["finished","-"*(common.cluster_protocol_plain_size - len('finished '))], 'master', True),
+    (["finished","-"*(common.cluster_protocol_plain_size - len('finished '))], 'client', False),
+    (["zip","3".zfill(common.cluster_protocol_plain_size - len('zip '))], 'client', True),
+    (["zip","3".zfill(common.cluster_protocol_plain_size - len('zip ')), "asdd"], 'client', False),
+    ], scope="session")
+def test_check_cluster_cmd(cmd, node_type, expected_output):
+    """
+    Checks multiple values for the check_cluster_cmd function
+    """
+    assert cluster.check_cluster_cmd(cmd, node_type) == expected_output
